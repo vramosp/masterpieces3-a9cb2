7202bbff97c7158bb3d60154517cfac1f7eb3534
Stackbit Code Editor <projects@stackbit.com>
2021-12-06 17:04:56 +0000

    sourcebit.js: changed

--- sourcebit.js
+++ sourcebit.js
@@ -57,6 +57,7 @@ module.exports = {
                                 urlPath,
                                 pageCssClasses: cssClassesFromUrlPath(urlPath)
                             },
+                            
                             ...restProps
                         };
                     });
