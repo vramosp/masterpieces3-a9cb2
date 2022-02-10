d155fbc429bd6a468e2089818297dc5ab997d503
Stackbit Code Editor <projects@stackbit.com>
2022-02-10 20:43:29 +0000

    static-props-resolvers.js: changed

--- src/utils/static-props-resolvers.js
+++ src/utils/static-props-resolvers.js
@@ -76,7 +76,7 @@ const StaticPropsResolvers = {
             ...props,
             ...paginationData,
             items,
-            layout: 'PostFeedCategoryLayout',
+            layout: 'PostFeedLayout',
             postFeed: {
                 showAuthor: true,
                 showDate: true,
