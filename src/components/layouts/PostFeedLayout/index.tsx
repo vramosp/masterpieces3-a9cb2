4f94772102de9d57115c50431d8345d2a2236236
Stackbit Code Editor <projects@stackbit.com>
2022-02-10 20:46:22 +0000

    index.tsx: changed

--- src/components/layouts/PostFeedLayout/index.tsx
+++ src/components/layouts/PostFeedLayout/index.tsx
@@ -21,7 +21,6 @@ export default function PostFeedLayout(props) {
                     </h1>
                 )}
                 {renderSections(topSections, 'topSections')}
-                <div>hola</div>
                 {renderSections(bottomSections, 'bottomSections')}
             </main>
         </BaseLayout>
