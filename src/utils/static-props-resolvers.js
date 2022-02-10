00b62b3394a5724ea89237fecf3868f1644c1690
Stackbit Code Editor <projects@stackbit.com>
2022-02-10 20:42:35 +0000

    static-props-resolvers.js: changed

--- src/utils/static-props-resolvers.js
+++ src/utils/static-props-resolvers.js
@@ -76,7 +76,7 @@ const StaticPropsResolvers = {
             ...props,
             ...paginationData,
             items,
-            layout: 'DefaultBaseLayout',
+            layout: 'BlankBaseLayout',
             postFeed: {
                 showAuthor: true,
                 showDate: true,
