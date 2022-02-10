96e249b1959fff31a0493913e5e1efbf13f8cb44
Stackbit Code Editor <projects@stackbit.com>
2022-02-10 21:03:51 +0000

    index.tsx: changed

--- src/components/sections/PostFeedSection/index.tsx
+++ src/components/sections/PostFeedSection/index.tsx
@@ -126,7 +126,7 @@ function postsVariantA(props) {
                     )}
                     <div className="px-4 py-6 sm:px-6 sm:pb-10">
                         {props.showDate && <PostDate post={post} />}
-                        <h1>
+                        <h2>
                             <Link href={getPageUrlPath(post)} data-sb-field-path="title">
                                 {post.title}
                             </Link>
