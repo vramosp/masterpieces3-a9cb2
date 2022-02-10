560621fd977b90327718aecb5516ee1ac194facc
Stackbit Code Editor <projects@stackbit.com>
2022-02-10 21:03:49 +0000

    index.tsx: changed

--- src/components/sections/PostFeedSection/index.tsx
+++ src/components/sections/PostFeedSection/index.tsx
@@ -130,7 +130,7 @@ function postsVariantA(props) {
                             <Link href={getPageUrlPath(post)} data-sb-field-path="title">
                                 {post.title}
                             </Link>
-                        </h1>
+                        </h2>
                     </div>
                 </article>
             ))}
